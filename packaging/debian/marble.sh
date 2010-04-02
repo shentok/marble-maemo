a5d680602b38627743c1bf7bc12483ec284c17c2
Dennis Nienhüser <nienhues@fzi.de>
2010-04-02 12:14:22 -0400

    Prepare for xdg paths: Data will be stored in /home/user/MyDocs/marble/data once marble supports it.

--- packaging/debian/marble.sh
+++ packaging/debian/marble.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# Set the environment variable XDG_DATA_HOME to have
+# marble store cached data in /home/user/MyDocs/marble/data
+# Can be overridden by specifying an alternative path in 
+# the environment variable MARBLE_DATA_HOME, e.g.
+# $ export MARBLE_DATA_HOME=/home/user
+# which would result in cached data being written to
+# /home/user/marble/data.
+XDG_DATA_HOME="${MARBLE_DATA_HOME:-/home/user/MyDocs}"
+export XDG_DATA_HOME
+
+# Marble is installed in /opt/marble. Add it's library
+# to the library search path.
 LD_LIBRARY_PATH="/opt/marble/lib:${LD_LIBRARY_PATH}"
 export LD_LIBRARY_PATH
+
 /opt/marble/bin/marble
